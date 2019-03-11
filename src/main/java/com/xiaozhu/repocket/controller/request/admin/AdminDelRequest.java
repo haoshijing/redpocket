2155065a0abe5853456a6f77f878de155be31c05
haoshijing <13336151702@163.com>
2019-03-11 15:22:17 +0800

    fix

--- src/main/java/com/xiaozhu/repocket/controller/request/admin/AdminDelRequest.java
+++ src/main/java/com/xiaozhu/repocket/controller/request/admin/AdminDelRequest.java
@@ -1,11 +1,10 @@
 package com.xiaozhu.repocket.controller.request.admin;
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
+
 import lombok.Data;
 
 @Data
-public class AdminDelRequest
-{
+public class AdminDelRequest {
     private Integer id;
     private String username;
 }
