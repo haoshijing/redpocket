01f67cc3edcfa7117305ead3e2b6494d201fa680
haoshijing <13336151702@163.com>
2019-03-11 16:03:43 +0800

    config方法修改

--- src/main/java/com/xiaozhu/repocket/controller/config/ConfigController.java
+++ src/main/java/com/xiaozhu/repocket/controller/config/ConfigController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("/api/config")
+@RequestMapping("/config")
 @RestController
 @Slf4j
 public class ConfigController extends BaseQueryRemoteController {
